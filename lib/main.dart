b429e50743c4ffb19d11f6d4ac1c393bbb3f7f8a
pannawit <pannawit.kittisoponpan@e-tech.ac.th>
2025-12-01 09:23:27 +0700

    "Final"

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        fontFamily: "NotoSansThai",
       ),
       initialRoute: '/',
       routes: {
@@ -65,7 +66,11 @@ class MyHomePage extends StatelessWidget {
                   SizedBox(height: 10),
                   Text(
                     "Pannawit Kittisoponpan",
-                    style: TextStyle(color: Colors.white, fontSize: 22,fontWeight: FontWeight.bold,)
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                   SizedBox(height: 2),
                   Text(
@@ -82,13 +87,13 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
 
                 children: [
-                  Text("ข้อมูลส่วนตัว"
-                    ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  Text(
+                    "ข้อมูลส่วนตัว",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
                   SizedBox(height: 20),
                   Row(
                     children: [
-
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
@@ -97,11 +102,17 @@ class MyHomePage extends StatelessWidget {
                         ),
                         child: Icon(Icons.phone, color: Colors.green),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                       Column(
-                        children: [Text("เบอร์โทรศัพท์"), Text("064-8625950", style: TextStyle(fontWeight: FontWeight.bold),)],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("เบอร์โทรศัพท์"),
+                          Text(
+                            "064-862-5950",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-
                     ],
                   ),
                   //อันที่2
@@ -116,11 +127,17 @@ class MyHomePage extends StatelessWidget {
                         ),
                         child: Icon(Icons.cake, color: Colors.pink),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                       Column(
-                        children: [Text("วันเกิด"), Text("2 มีนาคม 2549", style: TextStyle(fontWeight: FontWeight.bold))],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("วันเกิด"),
+                          Text(
+                            "2 มีนาคม 2549",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-
                     ],
                   ),
                   //3
@@ -130,16 +147,22 @@ class MyHomePage extends StatelessWidget {
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
-                          color: Colors.pink[100],
+                          color: Colors.orange[100],
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        child: Icon(Icons.cake, color: Colors.pink),
+                        child: Icon(Icons.location_on, color: Colors.orange),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                       Column(
-                        children: [Text("วันเกิด"), Text("2 มีนาคม 2549", style: TextStyle(fontWeight: FontWeight.bold))],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("ที่อยู่"),
+                          Text(
+                            "ชลบุรี",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-
                     ],
                   ),
                   //4
@@ -152,19 +175,32 @@ class MyHomePage extends StatelessWidget {
                           color: Colors.purple[100],
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        child: Icon(Icons.help_outline, color: Colors.purple),
+                        child: Icon(Icons.school, color: Colors.purple),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                       Column(
-                        children: [Text("การศึกษา"), Text("วิทยาลัยเทคโนโลยีภาคตะวันออก (อี.เทค)", style: TextStyle(fontWeight: FontWeight.bold))],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("การศึกษา"),
+                          Text(
+                            "วิทยาลัยเทคโนโลยีภาคตะวันออก (อี.เทค)",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-
                     ],
                   ),
                   SizedBox(height: 18),
-                  ElevatedButton(onPressed : () => Navigator.pushNamed(context, '/second'),
-                      child: Text('Go to Second', style: TextStyle(color: Colors.white),),
-                    style:ElevatedButton.styleFrom(backgroundColor: Colors.blue) ,
+                  ElevatedButton(
+                    onPressed: () => Navigator.pushNamed(context, '/second'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      fixedSize: Size(500, 20),
+                    ),
+                    child: Text(
+                      'ไปยังหน้า 2',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ],
               ),
@@ -181,6 +217,205 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Second Page")));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(4),
+                        child: ClipOval(
+                          child: Image.network(
+                            "https://i.pinimg.com/videos/thumbnails/originals/91/7a/6f/917a6fcd9bcf809563da549ddc2105b0.0000000.jpg",
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "24",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "กำลังติดตาม",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 1,
+                          horizontal: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.symmetric(
+                            vertical: BorderSide(
+                              color: Colors.grey.shade400,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              "179",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "ผู้ติดตาม",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "3",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "ถูกใจและบันทึก",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    child: Row(
+                      children: [
+                        Text(
+                          "Pannawit_Kittisoponpan ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(Icons.verified, color: Colors.blue),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.tiktok,
+                          color: Colors.grey.shade500,
+                          size: 13,
+                        ),
+                        Text(
+                          " pannawit31 ",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_drop_down,
+                          color: Colors.grey.shade500,
+                          size: 13,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  //ปุ่ม
+                  Container(
+                    child: Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () => Navigator.pushNamed(context, '#'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurpleAccent,
+                            fixedSize: Size(300, 20),
+                          ),
+                          child: Text(
+                            'ติดตาม',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Icon(Icons.share, color: Colors.black),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    child:
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(4),
+                                  child: Image.network(
+                                    "https://i.pinimg.com/videos/thumbnails/originals/91/7a/6f/917a6fcd9bcf809563da549ddc2105b0.0000000.jpg",
+                                    width: 180,
+                                    height: 180,
+                                    fit: BoxFit.cover,
+                                  ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(4),
+                                  child: Image.network(
+                                    "https://i.pinimg.com/videos/thumbnails/originals/91/7a/6f/917a6fcd9bcf809563da549ddc2105b0.0000000.jpg",
+                                    width: 180,
+                                    height: 180,
+                                    fit: BoxFit.cover,
+                                  ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
